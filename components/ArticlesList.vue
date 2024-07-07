7c0145d0fd2f2e3514262a1bc309bb73778bac06
Oleg Bondarenko <hlop4ik124@gmail.com>
2024-07-07 17:00:07 +0300

    ArticlesList dev

---
+++ components/ArticlesList.vue
@@ -0,0 +1,30 @@
+<template>
+	<div :style="{
+		gridTemplateCols: cols
+		gridTemplateRows: `repeat(${rowHeight}px, ${rows})`
+	}">
+		<ArticleItem v-for="article in articles" :article="article" />
+	</div>
+	<nav>
+		<!-- pagination -->
+	</nav>
+</template>
+
+<script setup lang="ts">
+	export interface Props {
+		cols?: number,
+		rows?: number
+	}
+
+	const rowHeight: number = 300;
+	const { cols, rows } = withDefaults(defineProps<Props>(), {
+		cols: 4,
+		rows: 2,
+	})
+
+	const { articles } = await useFetch('/articles');
+</script>
+
+<style scoped>
+	
+</style>
